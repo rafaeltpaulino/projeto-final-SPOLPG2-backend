4f5d19a9fb896813a160526ced426f9a757a0ee4
rafaeltpaulino <rafaeltpaulino5@gmail.com>
2025-11-30 18:00:26 -0300

    Alterações finais.

--- src/main/java/br/com/ifsp/backend/dto/response/view/UserLoginResponseDTO.java
+++ src/main/java/br/com/ifsp/backend/dto/response/view/UserLoginResponseDTO.java
@@ -1,4 +1,4 @@
 package br.com.ifsp.backend.dto.response.view;
 
-public record UserLoginResponseDTO(String token, Long userId) {
+public record UserLoginResponseDTO(String token, Long userId, String username) {
 }
--- src/main/java/br/com/ifsp/backend/service/user/AuthService.java
+++ src/main/java/br/com/ifsp/backend/service/user/AuthService.java
@@ -27,6 +27,6 @@ public class AuthService {
         User user = (User) authentication.getPrincipal();
         String token = tokenConfig.generateJWT(user);
 
-        return new UserLoginResponseDTO(token, user.getId());
+        return new UserLoginResponseDTO(token, user.getId(), user.getUsername());
     }
 }
